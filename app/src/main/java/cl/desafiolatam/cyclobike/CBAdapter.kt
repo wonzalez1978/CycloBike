677748f5bd08ff67e8873c43addc4d72eee5995c
wonzalez1978 <jaimegoklein@gmail.com>
2020-08-13 15:29:14 -0400

    adapter_viewHolder

---
+++ app/src/main/java/cl/desafiolatam/cyclobike/CBAdapter.kt
@@ -0,0 +1,20 @@
+package cl.desafiolatam.cyclobike
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+public class CBAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
