3293df6370b82a68c5a7db7aa16580c60466396f
wonzalez1978 <jaimegoklein@gmail.com>
2020-08-13 15:54:59 -0400

    hito 5.2

--- app/src/main/java/cl/desafiolatam/cyclobike/CBAdapter.kt
+++ app/src/main/java/cl/desafiolatam/cyclobike/CBAdapter.kt
@@ -1,20 +1,25 @@
 package cl.desafiolatam.cyclobike
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.ciromine.ciclobike.Ciclovia
 
-public class CBAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+public class CBAdapter(val list: MutableList<Ciclovia>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CycloBikeHolder<Any?> {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
+            return CycloBikeHolder(itemView)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return list.size
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         TODO("Not yet implemented")
     }
+    class CycloBikeHolder(itemView: View) : RecyclerView.ViewHolder<>
+
 
 }
